aa973676b685cf30d6211a66e70a51230537e5a7
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-07 11:00:57 +0100

    implemented search functionnality

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'connexion.php';
+require_once 'connexion.php';
 
 if(!isset($_GET["search"])){ 
     try {
@@ -8,25 +8,26 @@ if(!isset($_GET["search"])){
                 FROM hikes
                 ORDER BY id DESC';
     
-        $q = $db->query($sql);
+        $q = $pdo->query($sql);
         $q->setFetchMode(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         die("Could not connect to the database $dbname :" . $e->getMessage());
     }
         $all_hikes = $q->fetchAll(PDO::FETCH_ASSOC);
 } else {
+    $myQuery = "%{$_GET['search']}%";
     try {
-        $sql =  "SELECT *
-                 FROM hikes
-                 WHERE hike_name
-                 LIKE %{$_GET['search']}%";
-    
-        $q = $db->query($sql);
-        $q->setFetchMode(PDO::FETCH_ASSOC);
+        $searchsql =  $pdo->prepare('SELECT * FROM hikes WHERE hike_name LIKE ?');
+        $searchsql->execute([$myQuery]);
+        
+        
+        /* $searchq = $pdo->query($searchsql);
+        $searchq->setFetchMode(PDO::FETCH_ASSOC); */
     } catch (PDOException $e) {
-        die("Could not connect to the database $dbname :" . $e->getMessage());
+        die("Could not connect to the database $DB :" . $e->getMessage());
     }
-        $searchresult = $q->fetchAll(PDO::FETCH_ASSOC);
+    /* $searchresult = $searchq->fetchAll(PDO::FETCH_ASSOC); */
+    $searchresult = $searchsql->fetchAll();
 }
 ?>
 
@@ -34,35 +35,40 @@ if(!isset($_GET["search"])){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hiku</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-    <form action="index.php" method="get">
-        <input type="text" name="search" id="search">
-        <button type="submit">search</button>
-    </form>
-    <?php 
-    $arr = array(1 => "abc", 2 => "azerty");
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hiku</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <form action="index.php" method="get">
+            <input type="text" name="search" id="search">
+            <button type="submit">search</button>
+        </form>
+        <?php 
+    $arr = array(1 => "abc", 2 => "azerty"); ?>
+    <ul>
+        <?php
     if(!isset($_GET["search"])){
-        ?><ul> <?php
         
         foreach ($all_hikes as $item) { ?>
            <li><a href="./hike.php?id=<?php echo $item['id'] ?> "> <?php echo $item['hike_name']; ?></a> </li>
            <?php
         } ?>
-        </ul> <?php
-    } else { 
-        foreach ($searchresult as $item) { ?>
-            <li><a href="./hike.php?id=<?php echo $item['id'] ?> "> <?php echo $item['hike_name']; ?></a> </li>
+        <?php
+    } elseif (isset($_GET["search"])) { 
+        foreach ($searchresult as $searchitem) { 
+            foreach ($searchitem as $searchitemproperty)?>
+            <li><a href="./hike.php?id=<?php echo $searchitem["id"];?>"> 
+            <?php echo $searchitem["hike_name"]; ?>
+            </a></li>
             <?php
          }
-    }
-    ?>
-
-</body>
-</html>
+        }
+        ?>
+        </ul>
+        
+    </body>
+    </html>
