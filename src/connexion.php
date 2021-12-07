a65365cd71b2d5c129077befab8b2fe92a15248d
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-07 11:00:33 +0100

    changed variable name from db to pdo

--- src/connexion.php
+++ src/connexion.php
@@ -9,10 +9,10 @@ define("PASSWORD", "o[ncR4Q>VIV9VvN1(HH1");
 try {
     
     // We create a new instance of the class PDO
-    $db = new PDO("mysql:host=".HOST.";dbname=".DB.";port=".PORT, LOGIN, PASSWORD);
+    $pdo = new PDO("mysql:host=".HOST.";dbname=".DB.";port=".PORT, LOGIN, PASSWORD);
     
     //We want any issues to throw an exception with details, instead of a silence or a simple warning
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch(Exception $e) {
     // We intantiate an Exception object in $e so we can use methods within this object to display errors nicely
