e473b77f87a2f53ea49805f874059fb54563b7fd
raonix <raoni.gillet@gmail.com>
2021-12-08 11:33:53 +0100

    raoni edits

--- src/hike.php
+++ src/hike.php
@@ -1,5 +1,25 @@
 <?php 
-include 'header.php'
+include 'header.php';
+
+//echo $_GET['id'];
+
+require_once 'connexion.php';
+
+if(!isset($_GET["search"])){ 
+    try {
+        $sql = 'SELECT *
+                FROM hikes
+                WHERE hike_id = {$_GET["id"]}';
+    
+        $q = $pdo->query($sql);
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die("Could not connect to the database $dbname :" . $e->getMessage());
+    }
+        $all_hikes = $q->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
 ?>
 
 </body>
