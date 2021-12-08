4caa1c41221650cddec31feb65c548d9c78ab985
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-08 12:02:52 +0100

    shared

--- src/hike.php
+++ src/hike.php
@@ -6,17 +6,17 @@ echo $_GET['id'];
 require_once 'connexion.php';
 
 if(isset($_GET["id"])){ 
-    $myQuery = "%{$_GET['id']}%";
+    $myQuery = "{$_GET['id']}";
     try {
-        $searchsql =  $pdo->prepare('SELECT * FROM hikes WHERE hike_id LIKE ?');
+        $searchsql =  $pdo->prepare('SELECT * FROM hikes WHERE hike_id = ?');
         $searchsql->execute([$myQuery]);
     } catch (PDOException $e) {
         die("Could not connect to the database $DB :" . $e->getMessage());
     }
     $searchresult = $searchsql->fetchAll();
+    print_r($searchresult);
 }
-
-echo $item['hike_name']; ?>
+?>
 
 
 </body>
