06d62d1eea823e137494e2488ebad127e277687a
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-08 11:58:47 +0100

    removed useless code

--- src/hike.php
+++ src/hike.php
@@ -10,14 +10,9 @@ if(isset($_GET["id"])){
     try {
         $searchsql =  $pdo->prepare('SELECT * FROM hikes WHERE hike_id LIKE ?');
         $searchsql->execute([$myQuery]);
-        
-        
-        /* $searchq = $pdo->query($searchsql);
-        $searchq->setFetchMode(PDO::FETCH_ASSOC); */
     } catch (PDOException $e) {
         die("Could not connect to the database $DB :" . $e->getMessage());
     }
-    /* $searchresult = $searchq->fetchAll(PDO::FETCH_ASSOC); */
     $searchresult = $searchsql->fetchAll();
 }
 
