004d0d62b129b320d5d744faf1f923ede2e992c2
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-08 09:42:01 +0100

    included header in separate file

---
+++ src/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hiku</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <header>
+            <a href="index.php"><img src="./img/home.svg" alt="home icon" width="32px"></a>
+            <form action="index.php" method="get">
+                    <input type="text" name="search" id="search">
+                    <button type="submit">search</button>
+                </form>
+        </header>
--- src/index.php
+++ src/index.php
@@ -29,25 +29,9 @@ if(!isset($_GET["search"])){
     /* $searchresult = $searchq->fetchAll(PDO::FETCH_ASSOC); */
     $searchresult = $searchsql->fetchAll();
 }
-?>
 
+include 'header.php';
 
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hiku</title>
-        <link rel="stylesheet" href="./style.css">
-    </head>
-    <body>
-        <form action="index.php" method="get">
-            <input type="text" name="search" id="search">
-            <button type="submit">search</button>
-        </form>
-        <?php 
     $arr = array(1 => "abc", 2 => "azerty"); ?>
     <ul>
         <?php
@@ -62,7 +46,7 @@ if(!isset($_GET["search"])){
         foreach ($searchresult as $searchitem) { 
             foreach ($searchitem as $searchitemproperty)?>
             <li><a href="./hike.php?id=<?php echo $searchitem["id"];?>"> 
-            <?php echo $searchitem["hike_name"]; ?>
+            <h1><?php echo $searchitem["hike_name"]; ?></h1>
             </a></li>
             <?php
          }
