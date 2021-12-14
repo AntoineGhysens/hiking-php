0c70c292eebf8ac99cb25785f5286e2aeed1b939
AntoineGhysens <antoine.ghsens@gmail.com>
2021-12-14 14:10:08 +0100

    fixed favicon not loading

--- src/index.php
+++ src/index.php
@@ -1,18 +1,40 @@
 <?php
 
+function getDifficulty($param){
+    if($param == 1){
+        return 'Easy';
+    }
+    elseif($param == 2){
+        return 'Medium';
+    }
+    elseif($param == 3){
+        return 'Hard';
+    }
+    elseif($param == 4){
+        return 'Very hard';
+    }
+    elseif($param == 5){
+        return 'Extreme';
+    }
+};
+
+function itemContent($dist, $difficulty){
+    return $dist . ' km - ' . getDifficulty($difficulty) . ' by author';
+};
+
 require_once 'connexion.php';
 
 if(!isset($_GET["search"])){ 
     try {
-        $sql = 'SELECT * FROM hikes ORDER BY hike_name ASC';
-        # $sql = 'SELECT * FROM hikes ORDER BY hike_id DESC';
+        #$sql = 'SELECT * FROM hikes ORDER BY hike_name ASC'; # sort by alphabetic order
+        $sql = 'SELECT * FROM hikes ORDER BY hike_id DESC'; # sort by newest
     
         $q = $pdo->query($sql);
         $q->setFetchMode(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         die("Could not connect to the database $dbname :" . $e->getMessage());
     }
-        $all_hikes = $q->fetchAll(PDO::FETCH_ASSOC);
+    $all_hikes = $q->fetchAll(PDO::FETCH_ASSOC);
 } else {
     $myQuery = "%{$_GET['search']}%";
     try {
@@ -24,7 +46,19 @@ if(!isset($_GET["search"])){
     }
     $searchresult = $searchsql->fetchAll();
 }
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hiku</title>
+        <link rel="stylesheet" href="./style/style.css">
+        <link rel="icon" type="image/x-icon" href="./favicon.ico?v1">
+    </head>
+    <body>
+<?php
 include 'header.php';
 
     $arr = array(1 => "abc", 2 => "azerty"); ?>
@@ -33,15 +67,16 @@ include 'header.php';
     if(!isset($_GET["search"])){
         
         foreach ($all_hikes as $item) { ?>
-           <li><a href="./hike.php?id=<?php echo $item['hike_id'] ?> "> <?php echo $item['hike_name']; ?></a> </li>
+           <li><a href="./hike.php?id=<?php echo $item['hike_id'] ?> "> <?php echo $item['hike_name']; ?></a>
+           <p><?php echo itemContent($item['distance'], $item['hike_difficulty']); ?></p></li>
            <?php
         } ?>
         <?php
     } elseif (isset($_GET["search"])) { 
         foreach ($searchresult as $searchitem) { ?>
-            <li><a href="./hike.php?id=<?php echo $searchitem["hike_id"];?>"> 
-            <h1><?php echo $searchitem["hike_name"]; ?></h1>
-            </a></li> <?php
+            <li><a href="./hike.php?id=<?php echo $searchitem["hike_id"];?>"> <?php echo $searchitem["hike_name"]; ?></a>
+            <p><?php echo itemContent($item['distance'], $item['hike_difficulty']); ?></p></li> 
+            <?php
        }
     }
         ?>
